ecfb470e2a642a74ddb49e47bba04e2218f3f660
jakobsche <messages@jakobsche.de>
2019-05-12 22:20:50 +0200

    used a package for simple databases to simplify the TWorldEntity

--- universe.pas
+++ universe.pas
@@ -5,22 +5,42 @@ unit Universe;
 interface
 
 uses
-  Classes, SysUtils;
+  Classes, SysUtils, DBObjPas;
 
 type
 
-  TUniverse = class
+  TWorldEntity = class(TDataTable)
+  { an object in the universe }
+  private
 
+  public
+    property Entities[I: Integer]: TWorldEntity;
   end;
 
-var
-  TheUniverse: TUniverse;
+  { TUniverse }
+
+  TUniverse = class(TWorldEntity)
+  public
+    constructor Create(AnOwner: TComponent); override; {initializes the global
+      typed constant TheUniverse or replaces the instance, that it points to,
+      because only one global instance may exist}
+  end;
+
+const
+  TheUniverse: TUniverse = nil; {initialized by TUniverse.Create
+    The initialization is not done by this unit, because the owner of
+    TheUniverse can depend on the kind of application, that uses it}
 
 implementation
 
-initialization
+{ TUniverse }
 
-TheUniverse := TUniverse.Create;
+constructor TUniverse.Create(AnOwner: TComponent);
+begin
+  inherited Create(AnOwner);
+  if Assigned(TheUniverse) then TheUniverse.Free;
+  TheUniverse := Self
+end;
 
 end.
 
